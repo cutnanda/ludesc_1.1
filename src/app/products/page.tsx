c77d510e8416eaa30b9c3580ba88b1d24dca6761
cutnanda <cutnandanurbadriani@gmail.com>
2024-09-18 12:07:05 +0700

    1.1

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import Breadcrumb from '@/components/themebuilder/03-Breadcrumb';
 import NavSide from '@/components/themebuilder/2-NavSide/01-NavSide';
 import BorderNav from '@/components/themebuilder/2-NavSide/02-BorderNav';
 import ActiveMenu from '@/components/themebuilder/2-NavSide/03-ActiveMenu';
@@ -59,6 +60,57 @@ export default function ProductsPage() {
                         <BorderNav />
                         <NormalMenu number="04" href="/achievement" text="Achievement" />
                         <NormalMenu number="05" href="/team" text="Team" />
+                    </NavSide>
+                </div>
+            </div>
+            {/* <!-- Content --> */}
+            <div className="w-full flex justify-center items-center">
+                <div className="flex flex-col md:flex-row justify-center items-start p-5 w-[95%] lg:w-[90%] mb-[100px]">
+                    <div className="lg:hidden mb-3">
+                        <Breadcrumb
+                            link={{ name: "Home", href: "/" }}
+                            link2={{ name: "Introducing LUDESC" }}
+                        />
+                    </div>
+                    <div className="md:w-4/5 w-full flex flex-col md:flex-row justify-center items-start  ">
+                        {/* <!-- Item 2 (Image) --> */}
+                        <div className="w-full md:w-[55%] md:order-last flex justify-center items-center ml-5">
+                            <div className="w-[200px] sm:w-[400px]">
+                                <Image
+                                    src="/images/HeroImage.png"
+                                    alt="Hero Image"
+                                    width={700}
+                                    height={700}
+                                />
+                            </div>
+                        </div>
+
+                        {/* <!-- Item 1 (Text) --> */}
+                        <div className="w-full md:w-[45%] lg:mt-0 mt-8  ">
+                            <div>
+                                <h1 className="text-[#1C738E] text-[30px] lg:text-[46px] font-bold mb-[-5px] ">AI-Powered</h1>
+                                <p className="text-[#1c8e72] text-[23px] lg:text-[38px] font-bold">Lung Disease Screening & Telemedicine</p>
+                                <p className="text-[#828383] text-[15px] font-300 mb-5 mt-2 leading-[1.6rem]">
+                                    System to help patients and medical personnel in early screening for lung
+                                    disease with AI technology in the form of self-screening and telemedicine quickly, easy
+                                    and cheap.
+                                </p>
+                                <div className='flex items-center space-x-4'>
+                                    <button className="bg-[#1C738E] py-2.5 px-7 text-[16px] text-white font-semibold">Join
+                                        Now</button>
+                                    <button
+                                        className="text-[#1C738E] hover:text-[#ffffff] hover:bg-[#1C738E] py-2.5 px-3 text-[16px] font-medium flex justify-center">
+                                        <span className="material-symbols-outlined mr-3 text-[44px] ">
+                                            play_circle
+                                        </span>Watch Video
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <!-- NavSidebar --> */}
+                    <NavSide>
+                        <div></div>
                     </NavSide>
                 </div>
             </div>
